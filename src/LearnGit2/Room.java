2cf380e5f8271bcec2e43a9bc1aad211111fea1a
JohnBarnett9 <invictus@rams.colostate.edu>
2014-02-16 13:55:26 -0700

    Added comments about how to merge and resolve conflicts

--- src/LearnGit2/Room.java
+++ src/LearnGit2/Room.java
@@ -1,5 +1,18 @@
 /**
- * 
+ * Merge happens as follows:
+ * right click file that has red diamond on it
+ * select  team
+ * select merge tools
+ * select 2 side compare
+ * select make left side look like right side
+ * save file
+ * right click file that you merged
+ * select team
+ * select add to index
+ * right click project folder
+ * select team
+ * select commit
+ * commit and push the project
  */
 package LearnGit2;
 
